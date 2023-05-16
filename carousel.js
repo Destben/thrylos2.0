92d5a6071f13973fa103844592c4512353496f0e
FBQuail <70413929+FBQuail@users.noreply.github.com>
2023-05-15 23:32:31 -0500

    Carousel Code Works Now

--- carousel.js
+++ carousel.js
@@ -4,16 +4,26 @@ var slides_max = slides.length;
 var slides_selection = 0;
 var slide_change_interval = window.setInterval(change_slides, 3000)
 
-function change_slides() {
-    console.log(slides_selection)
-    slideshow.scrollTo(slides[slides_selection])
-    if (slides_selection + 1 < slides_max) {
-        slides_selection += 1
-    } else {
-        slides_selection = 0
-    }
-    console.log(slides[slides_selection])
+function on_ready() {
+    Array.prototype.forEach.call(slideshow.children, child => {
+        console.log(child)
+        child.style.display = 'none'
+    })
+    slides[0].style.display = 'block';
 }
 
+function change_slides() {
+    slides[slides_selection].style.display = 'none';
+    if (slides_selection + 1 < slides_max) {
+        slides_selection += 1
+        slides[slides_selection].style.display = 'block';
+    } else {
+        slides_selection = 0;
+        slides[0].style.display = 'block'
+        slides[slides_max].style.display = 'none';
+    }
+    
+}
 
+on_ready()
 console.log(slides[0])
