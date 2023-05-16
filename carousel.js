051f965143f9d650d2ca0e295f01b61f6ddd8cbb
FBQuail <70413929+FBQuail@users.noreply.github.com>
2023-05-15 23:08:57 -0500

    Part of the script is working

--- carousel.js
+++ carousel.js
@@ -1,7 +1,19 @@
 var slideshow = document.querySelector('.slideshow')
 var slides = slideshow.children;
+var slides_max = slides.length;
+var slides_selection = 0;
+var slide_change_interval = window.setInterval(change_slides, 3000)
+
+function change_slides() {
+    console.log(slides_selection)
+    slideshow.scrollTo(slides[slides_selection])
+    if (slides_selection + 1 < slides_max) {
+        slides_selection += 1
+    } else {
+        slides_selection = 0
+    }
+    console.log(slides[slides_selection])
+}
 
 
-
-
-console.log(slides)
+console.log(slides[0])
